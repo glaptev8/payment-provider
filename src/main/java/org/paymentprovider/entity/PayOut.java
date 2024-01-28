fe95103346ba7b8dd5c942948e33be1a2402d9c7
gleb laptev <gleb.laptev@etraveligroup.com>
2024-01-29 00:18:43 +0100

    payment-provider

--- src/main/java/org/paymentprovider/entity/PayOut.java
+++ src/main/java/org/paymentprovider/entity/PayOut.java
@@ -8,6 +8,7 @@ import org.paymentprovider.dto.CurrencyType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Table("payout")
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PayOut {
   @Id
   private Long id;
